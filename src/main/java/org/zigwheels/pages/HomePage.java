d50b5f6f7c72d40254b676420e082872334dd60c
S J <2497724@cognizant.com>
2026-07-01 10:51:42 +0530

    TC_12 added

--- src/main/java/org/zigwheels/pages/HomePage.java
+++ src/main/java/org/zigwheels/pages/HomePage.java
@@ -16,14 +16,28 @@ public class HomePage extends StandardCode {
     @FindBy(linkText = "Upcoming Bikes")
     WebElement upcomingBikes;
 
+    @FindBy(xpath = "//ul[contains(@class,'h-d-nav fnt')]/li[5]/span[contains(text(),'MORE')]")
+    WebElement moreMenu;
+
+    @FindBy(xpath = "//a[@title='Used Cars']")
+    WebElement usedCars;
+
     public void clickNewBikes() {
         clickElement(newBikes);
         System.out.println("NEW BIKES clicked");
     }
-
     public void clickUpcmngBikes() {
         waitForVisibility(upcomingBikes);
         clickElement(upcomingBikes);
         System.out.println("Upcoming Bikes clicked");
+    }
+    public void clickMore() {
+        moreMenu.click();
+    }
+
+    public void clickUsedCars() {
+        waitForVisibility(usedCars);
+        clickByJS(usedCars);
+        System.out.println("Used Cars clicked");
     }
 }
---
+++ src/test/java/org/zigwheels/test/TC_12GetUsedCars.java
@@ -0,0 +1,28 @@
+package org.zigwheels.test;
+
+import basetest.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.zigwheels.pages.HomePage;
+import utilities.Log;
+
+public class TC_12GetUsedCars extends BaseTest {
+    @Test
+    public void verifyUsedCarsURL()throws InterruptedException {
+
+        Log.info("Navigating to Used Cars page");
+
+        HomePage hp = new HomePage(driver);
+
+        hp.clickMore();
+        hp.clickUsedCars();
+
+        String expectedUrl = p.getProperty("getcarurl");
+        String actualUrl = driver.getCurrentUrl();
+
+        Assert.assertEquals(actualUrl, expectedUrl);
+
+        Log.info("Used Cars URL verification completed");
+        System.out.println("Used Cars URL verification completed");
+    }
+}
