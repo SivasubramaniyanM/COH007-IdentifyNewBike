9b3350caf3936e3c540dca4edb6cb7022c152811
SIVASUBRAMANIYAN M <152172345+SivasubramaniyanM@users.noreply.github.com>
2026-07-03 21:30:03 +0530

    Update TC_5DisplayHondaBikesUnder4Lakhs.java

--- src/test/java/org/zigwheels/test/TC_5DisplayHondaBikesUnder4Lakhs.java
+++ src/test/java/org/zigwheels/test/TC_5DisplayHondaBikesUnder4Lakhs.java
@@ -1,25 +1,46 @@
 package org.zigwheels.test;
+
 import basetest.BaseTest;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.zigwheels.pages.BikesPage;
 import org.zigwheels.pages.HomePage;
+import utilities.ExportVehicleDetails;
 import utilities.Log;
 
+import java.io.IOException;
+import java.util.List;
+
 public class TC_5DisplayHondaBikesUnder4Lakhs extends BaseTest {
+
     @Test
-    public void displayHondaBikesUnder4Lakhs() {
+    public void displayHondaBikesUnder4Lakhs() throws IOException {
+
         Log.info("Opening Honda Bikes Page");
+
         HomePage hp = new HomePage(driver);
         hp.clickNewBikes();
         hp.clickUpcmngBikes();
+
         BikesPage bp = new BikesPage(driver);
         bp.clickHondaBrand();
+
         Assert.assertTrue(
                 driver.getCurrentUrl().contains("honda"),
                 "Honda page is not opened"
         );
-        bp.getHondaBikesUnder4Lakhs();
-        Log.info("Honda Bikes under 4 Lakhs displayed successfully");
+
+        List<String> names = bp.getHondaBikeNamesUnder4Lakhs();
+        List<String> prices = bp.getHondaBikePricesUnder4Lakhs();
+        List<String> dates = bp.getHondaBikeDatesUnder4Lakhs();
+
+        Assert.assertFalse(
+                names.isEmpty(),
+                "No Honda bikes under 4 Lakhs found"
+        );
+
+        ExportVehicleDetails.writeBikeDetailsToExcel(names, prices, dates);
+
+        Log.info("Honda Bikes stored in Vehicle.xlsx successfully");
     }
 }
