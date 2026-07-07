90c03bd2b81494407445d0c01e4502dcbccd8e5e
SIVASUBRAMANIYAN M <152172345+SivasubramaniyanM@users.noreply.github.com>
2026-07-07 17:03:35 +0530

    Update TC_7UnrevealedLaunchBike.java

--- src/test/java/org/zigwheels/test/TC_7UnrevealedLaunchBike.java
+++ src/test/java/org/zigwheels/test/TC_7UnrevealedLaunchBike.java
@@ -16,12 +16,12 @@ public class TC_7UnrevealedLaunchBike extends BaseTest {
         hp.clickNewBikes();
         hp.clickUpcmngBikes();
         BikesPage bp = new BikesPage(driver);
-<<<<<<< Updated upstream
+
         bp.clickHondaBrand();
         Assert.assertTrue(driver.getCurrentUrl().contains("honda"), "Honda page is not opened");
         List<String> unrevealedBikes = bp.getUnrevealedLaunchDateBikes();
         Log.info("Total unrevealed Honda bikes under 4 Lakhs : " + unrevealedBikes.size());
-=======
+
         //  Click Honda brand filter
         bp.clickHondaBrand();
         Assert.assertTrue(
@@ -31,16 +31,14 @@ public class TC_7UnrevealedLaunchBike extends BaseTest {
         List<String> unrevealedBikes = bp.getUnrevealedLaunchDateBikes();
         Log.info("Total unrevealed Honda bikes under 4 Lakhs : " + unrevealedBikes.size());
         System.out.println("Honda Bikes with Unrevealed Launch Date (Under 4 Lakhs) : ");
->>>>>>> Stashed changes
+
         for (String bike : unrevealedBikes) {
             System.out.println(" -> " + bike);
             Log.info("Unrevealed Honda bike : " + bike);
         }
-<<<<<<< Updated upstream
+
         Assert.assertNotNull(unrevealedBikes, p.getProperty("bikenotnull"));
-=======
         Assert.assertNotNull(unrevealedBikes, "Bike list should not be null");
->>>>>>> Stashed changes
         Log.info("Honda unrevealed launch date bikes verification completed");
     }
 }
