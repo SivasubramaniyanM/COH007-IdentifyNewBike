742dc7b1fcb108af1bf871094d73d32b8b2d1c47
SIVASUBRAMANIYAN M <152172345+SivasubramaniyanM@users.noreply.github.com>
2026-07-08 21:18:33 +0530

    Update TC_8HeroDealerDetails.java

--- src/test/java/org/zigwheels/test/TC_8HeroDealerDetails.java
+++ src/test/java/org/zigwheels/test/TC_8HeroDealerDetails.java
@@ -9,13 +9,14 @@ import org.zigwheels.pages.HomePage;
 
 public class TC_8HeroDealerDetails extends BaseTest {
     @Test
-    public void heroDealerDetails() {
+    public void heroDealerDetails()throws Exception{
         HomePage hp = new HomePage(driver);
         BikesPage bp = new BikesPage(driver);
         hp.clickNewBikes();
         hp.clickBikeDealers();
         bp.clickChennai();
         bp.clickHeroMotoCorp();
+        takeScreenShot(driver, "Hero Dealer Details");
         bp.getHeroDealerDetails();
         Assert.assertTrue(driver.getPageSource().contains("Hero"), p.getProperty("nopageload"));
         logger.info("Hero dealer details extracted successfully");
